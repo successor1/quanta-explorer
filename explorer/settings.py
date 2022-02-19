c272e1d9a5c1f25ffba2786ca6bf49e76f0aba02
mob <54854891+successor1@users.noreply.github.com>
2022-02-19 23:53:18 +0100

    Update settings.py

--- explorer/settings.py
+++ explorer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mo0wyz$0mdi)jo+-!cs(d-nmikd1b&otrkytly^%5@fk6za-9x'
+SECRET_KEY = 'SECRET_KEY_HERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
